3d109e418f7f975013cfd88d7a9e1cab223a8330
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-10-26 18:29:18 +0200

    Updated settings

--- website/settings.py
+++ website/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles' # this is used in production to collect all static files
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
