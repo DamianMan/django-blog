e2b2686ae1a3fe51ef46cf030e26b230bd71ad3c
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-10-26 17:40:20 +0200

    Updated db settings

--- website/settings.py
+++ website/settings.py
@@ -87,10 +87,8 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
 }
 
 
