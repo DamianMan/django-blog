5dc7874ddb52e010abaed43862f1031719f0833e
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-10-26 17:45:21 +0200

    Updated build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Convert static asset files
 python3 manage.py collectstatic --no-input
